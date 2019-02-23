5426159fe468595a4ef02c3f6035f2221096837b
nolan <nolanbconaway@gmail.com>
2019-02-23 17:46:03 -0500

    add database trimmer

---
+++ trim-database.py
@@ -0,0 +1,20 @@
+"""Remove MTA snapshot records without a rating.
+
+Basically to save space. I'm not _made of money_.
+"""
+
+from app.models import db, Snapshot
+
+sql = """
+SELECT snapshot_id
+FROM rating_snapshot_map
+WHERE rating_id IS NULL
+"""
+
+# delete em
+for (snapshot_id,) in db.session.execute(sql):
+    snapshot = Snapshot.query.get(id)
+    db.session.delete(snapshot)
+
+# commit
+db.session.commit()
