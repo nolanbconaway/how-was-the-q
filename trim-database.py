a598de21a0a219d1d12ef2eceb0ccd98fda5a90f
nolan <nolanbconaway@gmail.com>
2019-02-23 18:14:43 -0500

    fix timezone bug

--- trim-database.py
+++ trim-database.py
@@ -7,22 +7,18 @@ from app.models import db, Snapshot
 import datetime
 
 # query a view of ratings outer joined to snapshots
-sql = """
-SELECT snapshot_id
-FROM rating_snapshot_map
-WHERE rating_id IS NULL
-"""
+sql = "SELECT snapshot_id FROM rating_snapshot_map WHERE rating_id IS NULL"
 
 seconds_in_day = 86400
 
 # delete em
 for (snapshot_id,) in db.session.execute(sql).fetchall():
     snapshot = Snapshot.query.get(snapshot_id)
-    age_seconds = (datetime.datetime.now() - snapshot.snapshot_dttm).total_seconds()
+    age_delta = datetime.datetime.utcnow() - snapshot.snapshot_dttm
 
-    # only delete after 24h. The view only joins if the rating is within 18 but i think
-    # there might be a timezone bug so lets keep em around for a day.
-    if age_seconds < seconds_in_day:
+    # only delete after 24h. The view only joins if the rating is within 18 but I like
+    # having a little slack.
+    if age_delta.total_seconds() < seconds_in_day:
         continue
     else:
         db.session.delete(snapshot)
